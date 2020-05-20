93158c87fa986a1437ee9264785c28e98b5afc96
Simran <simrangil96@gmsil.com>
2020-05-20 12:25:11 -0700

    new changes made

---
+++ src/myJavaProject/myAppTester.java
@@ -0,0 +1,16 @@
+package myJavaProject;
+
+import java.util.Scanner;
+
+public class myAppTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input=new Scanner(System.in);
+		int input1;
+		System.out.println("Enter an even number or 999 to stop");
+		input1=input.nextInt();
+	}
+
+}
